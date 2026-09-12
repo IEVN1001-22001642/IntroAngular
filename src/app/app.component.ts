10847185f192456ae338a80fc4f03512c019f51f
a.mendez <sillywhisp@gmail.com>
2026-09-12 09:59:32 -0600

    Heroes 12/09/2026

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'IntroAngular';
+  title = 'John Fortnite';
+
+  duplicaNumero(n1:number):number{
+    return n1*2
+  }
+
+  pelicula={
+    titulo:'Spider Man',
+    fechaLanzamiento:new Date(),
+    precio:1234
+  }
 }
---
+++ src/app/heroes/heroes-list/heroes-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list',
+  standalone: false,
+  templateUrl: './heroes-list.component.html'
+})
+export class HeroesListComponent {
+
+}
